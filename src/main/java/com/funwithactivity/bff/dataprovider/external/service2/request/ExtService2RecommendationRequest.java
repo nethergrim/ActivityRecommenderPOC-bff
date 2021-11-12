7b8f7cfd285af9d829962e900a5e14a0274b8eb0
Andrii Drobiazko <drobia01@kits.kfplc.com>
2021-11-12 13:41:06 +0200

    data parsing fallback

--- src/main/java/com/funwithactivity/bff/dataprovider/external/service2/request/ExtService2RecommendationRequest.java
+++ src/main/java/com/funwithactivity/bff/dataprovider/external/service2/request/ExtService2RecommendationRequest.java
@@ -2,13 +2,15 @@ package com.funwithactivity.bff.dataprovider.external.service2.request;
 
 import com.funwithactivity.bff.models.RecommendationRequest;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class ExtService2RecommendationRequest {
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
-            .ofPattern("M/d/yyyy");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("M/d/yyyy");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER_FALLBACK = DateTimeFormatter.ofPattern("M/d/yy");
+    private static final long SECONDS_IN_DAY = 24 * 60 * 60;
     private final Measurements measurements;
     private final String session_token;
     private final long birth_date;
@@ -18,12 +20,17 @@ public class ExtService2RecommendationRequest {
         this.measurements = new Measurements(anotherRequest.getBodyWeight(), anotherRequest.getBodyHeight());
 
         String dateString = anotherRequest.getBirthdate();
-        if (dateString != null) {
-            LocalDate date = LocalDate.parse(dateString, DATE_TIME_FORMATTER);
-            this.birth_date = date.toEpochDay() * 24 * 60 * 60;
-        } else {
+        if (dateString == null) {
             this.birth_date = 0;
+            return;
         }
+        long birthdateTimeStamp;
+        try {
+            birthdateTimeStamp = LocalDate.parse(dateString, DATE_TIME_FORMATTER).toEpochDay() * SECONDS_IN_DAY;
+        } catch (DateTimeException e) {
+            birthdateTimeStamp = LocalDate.parse(dateString, DATE_TIME_FORMATTER_FALLBACK).toEpochDay() * SECONDS_IN_DAY;
+        }
+        this.birth_date = birthdateTimeStamp;
     }
 
     public ExtService2RecommendationRequest(Measurements measurements, String session, long birthDate) {
